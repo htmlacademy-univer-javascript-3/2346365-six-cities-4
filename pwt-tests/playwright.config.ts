360165e55fa5101beef97f1fe49ec01c2dd3e0c8
kursor <sergey.kurochkin@mobileup.ru>
2024-05-26 00:42:55 +0300

    module9-task1

--- pwt-tests/playwright.config.ts
+++ pwt-tests/playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
   //Run your local dev server before starting the tests */
   webServer: {
     command: 'cd .. && npm run start',
-    url: 'http://127.0.0.1:5173',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
 });
